ffa7b1be577e5771cdb50503ebfa51a85b7c2a9f
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 19:39:53 -0500

    [red] fizzbuzz fails to convert 20 to Buzz

--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -9,6 +9,7 @@ test.each([
     [9, "Fizz"],
     [5, "Buzz"],
     [10,"Buzz"]
+    [20,"Buzz"]
 ])('should convert %p to %p', (number, expected) => {
     const result = fizzBuzz(number)
     expect(result).toBe(expected)
