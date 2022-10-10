af8a502fff6c966a3ecc63505be42c187518de5f
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 19:30:53 -0500

    [red] fizzbuzz fails to convert 6 to Fizz

--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -4,7 +4,8 @@ test.each([
     [1, "1"],
     [2, "2"],
     [4, "4"],
-    [3, "Fizz"]
+    [3, "Fizz"],
+    [6, "Fizz"]
 ])('should convert %p to %p', (number, expected) => {
     const result = fizzBuzz(number)
     expect(result).toBe(expected)
