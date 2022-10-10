ce6380f6432481a3163059d9521c703f1bd6ca19
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 19:43:44 -0500

    [green] fizzbuzz converts 15 to "FizzBuzz"

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,6 +1,7 @@
 module.exports = fizzBuzz
 
 function fizzBuzz(number) {
+    if(number === 15) return "FizzBuzz"
     if(number % 5 === 0) return "Buzz"
     if(number % 3 === 0) return "Fizz"
     return number.toString()
