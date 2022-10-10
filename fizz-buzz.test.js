bec1b5ad8cf6476ffab8eb2e4b5db550f58bcdd8
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 19:29:09 -0500

    [red] fizzBuzz fails to convert 3 to Fizz

--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -3,7 +3,8 @@ const fizzBuzz = require('./fizz-buzz')
 test.each([
     [1, "1"],
     [2, "2"],
-    [4, "4"]
+    [4, "4"],
+    [3, "Fizz"]
 ])('should convert %p to %p', (number, expected) => {
     const result = fizzBuzz(number)
     expect(result).toBe(expected)
