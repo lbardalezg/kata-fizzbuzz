9406c4578da55d3a7151232a81ff5a74e29cc1dd
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 19:39:27 -0500

    [green] fizzbuzz converts 10 to Buzz

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,7 +1,7 @@
 module.exports = fizzBuzz
 
 function fizzBuzz(number) {
-    if(number === 5) return "Buzz"
+    if(number === 5 || number === 10) return "Buzz"
     if(number % 3 === 0) return "Fizz"
     return number.toString()
 }
